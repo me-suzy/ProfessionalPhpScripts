815a6b12d0857b1b8af6b856fc7b937af767b140
me-suzy <me.suzana@gmail.com>
2025-08-10 06:27:34 +0300

    Add banners/templates/Plain/header.php

---
+++ banners/templates/Plain/header.php
@@ -0,0 +1,7 @@
+<html>
+<head><title><? echo"$sitename"; ?></title>
+</head>
+<body text="#000000" link="#ff0000" vlink="#ff0000" alink="#ff0000" bgcolor="#ffffff">
+<?php echo $message; ?>
+<center><font color="#ff0000" size="7"><? echo"$sitename"; ?></font></center>
+<HR>
