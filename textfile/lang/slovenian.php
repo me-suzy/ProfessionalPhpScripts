20e36f9e4808603cb85900bb9e12842b850c7d44
me-suzy <me.suzana@gmail.com>
2025-08-10 11:55:04 +0300

    Add textfile/lang/slovenian.php

---
+++ textfile/lang/slovenian.php
@@ -0,0 +1,182 @@
+<?php
+
+# Advanced Poll Language File (Admin)					#
+# Slovenian translation by Martin McDowell, dr.vet.med.	#
+# E-mail: martin.mcdowell@email.si						#
+# Date: 03/01/2002										#
+
+/* Charset */
+$lang["charset"]   = "windows-1250";
+
+/* General */
+$lang["Logout"]    = "Odjava";
+$lang["FormUndo"]  = "Razveljavi spremembe";
+$lang["FromClear"] = "Poèisti";
+$lang["FormEnter"] = "Vnesite veljavno uporabniko ime in geslo";
+$lang["FormWrong"] = "Napaèno uporabniko ime ali geslo";
+$lang["FormOK"]    = "V redu";
+$lang["Updated"]   = "Spremembe so shranjene!";
+$lang["NoUpdate"]  = "Napaka! Spremembe se niso upotevale!";
+$lang["Confirm"]   = "Ali ste preprièani?";
+$lang["NavNext"]   = "Naslednja stran";
+$lang["NavPrev"]   = "Prejnja stran";
+$lang["License"]   = "Licenèna pogodba";
+$lang["ScrollTxt"] = "Pritisnite gumb NASLEDNJA STRAN za ogled celotne pogodbe.";
+
+/* Templates */
+$lang["Templates"]  = "Predloge";
+$lang["tpl_exist"]  = "Ta predloga e obstaja.";
+$lang["tpl_new"]    = "Dodaj novo predlogo.";
+$lang["tpl_succes"] = "Zapis uspeno dodan!"; 
+$lang["tpl_bad"]    = "Ime predloge neveljavno!";
+$lang["tpl_save"]   = "Shrani";
+$lang["preview"]    = "Predogled";
+$lang["newtpl"]     = "Nova predloga";
+
+/* Poll List */
+$lang["IndexTitle"]  = "Spisek anket";
+$lang["IndexQuest"]  = "Vpraanje";
+$lang["IndexID"]     = "ID ankete";
+$lang["IndexDate"]   = "Datum";
+$lang["IndexDays"]   = "Dni";
+$lang["IndexExp"]    = "Anketa poteèe dne";
+$lang["IndexExpire"] = "Anketa konèana.";
+$lang["IndexNever"]  = "nikoli";
+$lang["IndexStat"]   = "Statistika";
+$lang["IndexCom"]    = "Komentarji";
+$lang["IndexAct"]    = "Dejanje";
+$lang["IndexDel"]    = "zbrii";
+
+/* Create A New Poll */
+$lang["NewTitle"]  = "Ustvari novo anketo";
+$lang["NewOption"] = "Opcija";
+$lang["NewNoQue"]  = "Pozabili ste vnesti vpraanje";
+$lang["NewNoOpt"]  = "Pozabili ste vnesti opcijo";
+
+/* Poll Edit */
+$lang["EditStat"]  = "Status";
+$lang["EditText"]  = "Uredi anketo";
+$lang["EditReset"] = "Ponastavi anketo";
+$lang["EditOn"]    = "omogoèeno";
+$lang["EditOff"]   = "onemogoèeno";
+$lang["EditHide"]  = "skrito";
+$lang["EditLgOff"] = "odjavljanje";
+$lang["EditLgOn"]  = "prijavljanje";
+$lang["EditAdd"]   = "Dodaj monosti";
+$lang["EditNo"]    = "Nobena monost ni bila dodana!";
+$lang["EditOk"]    = "Monosti so bile dodane!";
+$lang["EditSave"]  = "Shrani spremembe";
+$lang["EditOp"]    = "Zahtevani st anajmanj dve monosti!";
+$lang["EditMis"]   = "Vpraanja in monosti niso definirani.!";
+$lang["EditDel"]   = "Da odstranite monost pustite polje Monost prazno.";
+$lang["EditCom"]   = "Dovoli komentarje";
+
+/* General Settings */
+$lang["SetTitle"]   = "Splone Nastavitve";
+$lang["SetOption"]  = "Monosti Tabele, Pisave (fonta) ter Barv";
+$lang["SetMisc"]    = "Razno";
+$lang["SetText"]    = "Uredi splone nastavitve";
+$lang["SetURL"]     = "URL naslov za mapo z grafiko";
+$lang["SetBURL"]    = "URL naslov za mapo z anketo";
+$lang["SetNo"]      = "Brez \ oz. / na koncu";
+$lang["SetLang"]    = "Jezik";
+$lang["SetPoll"]    = "Naslov ankete";
+$lang["SetButton"]  = "Gumb za glasovanje";
+$lang["SetResult"]  = "Povezava za rezultate";
+$lang["SetVoted"]   = "Ste e glasovali";
+$lang["SetComment"] = "Poljite va komentar";
+$lang["SetTab"]     = "irina Tabele";
+$lang["SetBarh"]    = "Viina èrte (rezultatov)";
+$lang["SetBarMax"]  = "Najveèja dolnina èrte";
+$lang["SetTabBg"]   = "Barva ozadja tabele";
+$lang["SetFrmCol"]  = "Barva okvirja";
+$lang["SetFontCol"] = "Barva pisave (fonta)";
+$lang["SetFace"]    = "Oblika pisave (font)";
+$lang["SetShow"]    = "Prikai rezultat kot";
+$lang["SetPerc"]    = "odstotke";
+$lang["SetVotes"]   = "glasove";
+$lang["SetCheck"]   = "Preverjanje (check)";
+$lang["SetNoCheck"] = "brez preverjanja (no checking)";
+$lang["SetIP"]      = "IP tabela";
+$lang["SetTime"]    = "locking timeout";
+$lang["SetHours"]   = "ure";
+$lang["SetOffset"]  = "Server time offset";
+$lang["SetEntry"]   = "tevilo komentarjev na stran";
+$lang["SetSubmit"]  = "Polji Nastavitve";
+$lang["SetEmpty"]   = "Nepravilna (invalid) vrednost";
+$lang["SetSort"]    = "Vrstni red prikaza";
+$lang["SetAsc"]     = "naraèajoèe";
+$lang["SetDesc"]    = "padajoèe";
+$lang["Setusort"]   = "ne razvrèaj";
+$lang["SetOptions"] = "Nastavitve dovoljene v novih anketah";
+$lang["SetPolls"]   = "tevilo anket na stran";
+
+/* Change Password */
+$lang["PwdTitle"] = "Spremeni geslo";
+$lang["PwdText"]  = "Spremenite uporabniko ime ali geslo";
+$lang["PwdUser"]  = "Uporabniko ime";
+$lang["PwdPass"]  = "Geslo";
+$lang["PwdConf"]  = "Potrdite Geslo";
+$lang["PwdNoUsr"] = "Pozabili ste vnesti uporabniko ime";
+$lang["PwdNoPwd"] = "Pozabili ste vnesti geslo";
+$lang["PwdBad"]   = "Vpisani gesli se ne ujemata!";
+
+/* Poll Stats */
+$lang["StatCrea"]  = "Ustvarjeno";
+$lang["StatAct"]   = "Aktivno";
+$lang["StatReset"] = "Ponastavite log datoteko za statistiko";
+$lang["StatDis"]   = "logiranje je onemogoèeno za to anketo";
+$lang["StatTotal"] = "Skuno tevilo glasovanj";
+$lang["StatDay"]   = "Skupno tevilo dnevnih glasovanj";
+
+/* Poll Comments */
+$lang["ComTotal"]  = "Vsi komentarji";
+$lang["ComName"]   = "Ime in priimek";
+$lang["ComPost"]   = "poslano";
+$lang["ComDel"]    = "Ste preprièani, da elite izbrisati to sporoèilo?";
+
+/* Help */
+$lang["Help"]       = "Pomoè";
+$lang["HelpPoll"]   = "Za vkljuèitev ankete v spletno stran vstavite spodnjo kodo.";
+$lang["HelpRand"]   = "Monost imate tudi prikaz nakljuèno izbrane ankete.";
+$lang["HelpNew"]    = "Vedno prikai najnovejo anketo.";
+$lang["HelpSyntax"] = "Sintaksa";
+
+/* Days */
+$weekday[0] = "nedelja";
+$weekday[1] = "ponedeljek";
+$weekday[2] = "torek";
+$weekday[3] = "sreda";
+$weekday[4] = "èetrtek";
+$weekday[5] = "petek";
+$weekday[6] = "sobota";
+
+/* Months */
+$months[0]  = "januar";
+$months[1]  = "februar";
+$months[2]  = "marec";
+$months[3]  = "april";
+$months[4]  = "maj";
+$months[5]  = "junij";
+$months[6]  = "julij";
+$months[7]  = "avgust";
+$months[8]  = "september";
+$months[9]  = "oktober";
+$months[10] = "november";
+$months[11] = "december";
+
+/* Colors */
+$color_array[0]  = "aqua";
+$color_array[1]  = "blue";
+$color_array[2]  = "brown";
+$color_array[3]  = "darkgreen";
+$color_array[4]  = "gold";
+$color_array[5]  = "green";
+$color_array[6]  = "grey";
+$color_array[7]  = "orange";
+$color_array[8]  = "pink";
+$color_array[9]  = "purple";
+$color_array[10] = "red";
+$color_array[11] = "yellow";
+
+?>
