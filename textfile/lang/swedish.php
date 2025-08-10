c66dd13b4b3b34086b24f8c2a54f72f982906b21
me-suzy <me.suzana@gmail.com>
2025-08-10 11:55:07 +0300

    Add textfile/lang/swedish.php

---
+++ textfile/lang/swedish.php
@@ -0,0 +1,177 @@
+<?php
+
+/* Charset */
+$lang["charset"]   = "iso-8859-1";
+
+/* General */
+$lang["Logout"]    = "Logga Ut";
+$lang["FormUndo"]  = "Gör inga ändringar";
+$lang["FromClear"] = "Rensa";
+$lang["FormEnter"] = "Var god och skriv rätt användarnamn och lösenord";
+$lang["FormWrong"] = "Fel användarnamn och lösenord";
+$lang["FormOK"]    = "OK";
+$lang["Updated"]   = "Ändringarna har uppdaterats!";
+$lang["NoUpdate"]  = "Ett fel har uppstått! Inga ändringar har gjorts!";
+$lang["Confirm"]   = "Är du säker?";
+$lang["NavNext"]   = "Nästa sida";
+$lang["NavPrev"]   = "Föregående sida";
+$lang["License"]   = "Licens avtal";
+$lang["ScrollTxt"] = "Tryck på PAGE DOWN knappen för att läsa resten av avtalet.";
+
+/* Templates */
+$lang["Templates"]  = "Mallar";
+$lang["tpl_exist"]  = "Namnet på mallen existerar redan.";
+$lang["tpl_new"]    = "Lägg till en ny uppsättning av mallar.";
+$lang["tpl_succes"] = "Mallen lades till utan problem!"; 
+$lang["tpl_bad"]    = "Namnet på mallen är ogiltig!";
+$lang["tpl_save"]   = "Spara";
+$lang["preview"]    = "Förhandsgranska";
+$lang["newtpl"]     = "Ny Uppsättning Av Mallar";
+
+/* Poll List */
+$lang["IndexTitle"]  = "Lista över alla omröstningar";
+$lang["IndexQuest"]  = "Fråga";
+$lang["IndexID"]     = "Omröstnings ID";
+$lang["IndexDate"]   = "Datum";
+$lang["IndexDays"]   = "Dagar";
+$lang["IndexExp"]    = "Förfaller";
+$lang["IndexExpire"] = "förfallit";
+$lang["IndexNever"]  = "aldrig";
+$lang["IndexStat"]   = "Statistik";
+$lang["IndexCom"]    = "Kommentar";
+$lang["IndexAct"]    = "Åtgärd";
+$lang["IndexDel"]    = "radera";
+
+/* Create A New Poll */
+$lang["NewTitle"]  = "Skapa en ny omröstning";
+$lang["NewOption"] = "Val";
+$lang["NewNoQue"]  = "Du glömde att fylla i fältet för frågan";
+$lang["NewNoOpt"]  = "Du glömde att fylla i val fältet";
+
+/* Poll Edit */
+$lang["EditStat"]  = "Status";
+$lang["EditText"]  = "Ändra denna omröstning";
+$lang["EditReset"] = "Starta denna omröstning på nytt";
+$lang["EditOn"]    = "på";
+$lang["EditOff"]   = "av";
+$lang["EditHide"]  = "gömd";
+$lang["EditLgOff"] = "logga ut";
+$lang["EditLgOn"]  = "logga in";
+$lang["EditAdd"]   = "Lägg till val";
+$lang["EditNo"]    = "Inga val har lagts till!";
+$lang["EditOk"]    = "Val har lagts till!";
+$lang["EditSave"]  = "Spara Ändringar";
+$lang["EditOp"]    = "Du måste ha minst två val med!";
+$lang["EditMis"]   = "Fråga och val är inte exakt!";
+$lang["EditDel"]   = "För att ta bort ett val se till att rutan är tom";
+$lang["EditCom"]   = "Tillåt kommentarer";
+
+/* General Settings */
+$lang["SetTitle"]   = "Huvud Inställningar";
+$lang["SetOption"]  = "Tabell, Font/Teckensnitt och Färg val";
+$lang["SetMisc"]    = "Blandat";
+$lang["SetText"]    = "Ändra huvud Inställningarna";
+$lang["SetURL"]     = "URL till bild katalogen";
+$lang["SetBURL"]    = "URL till omröstnings katalogen";
+$lang["SetNo"]      = "Ingen trailing slash";
+$lang["SetLang"]    = "Språk";
+$lang["SetPoll"]    = "Rubrik/Titel på omröstningen";
+$lang["SetButton"]  = "Röst knapp";
+$lang["SetResult"]  = "Resultat länk";
+$lang["SetVoted"]   = "Har redan röstat";
+$lang["SetComment"] = "Skicka din kommentar";
+$lang["SetTab"]     = "Tabell bredd";
+$lang["SetBarh"]    = "Bar höjd";
+$lang["SetBarMax"]  = "Max. bar längd";
+$lang["SetTabBg"]   = "Bakgrunds färg på tabellen";
+$lang["SetFrmCol"]  = "Frame/Ram färg";
+$lang["SetFontCol"] = "Font/Teckensnitt färg";
+$lang["SetFace"]    = "Font/Teckensitt face";
+$lang["SetShow"]    = "Visa resultat i";
+$lang["SetPerc"]    = "procent";
+$lang["SetVotes"]   = "röster";
+$lang["SetCheck"]   = "Kontroll";
+$lang["SetNoCheck"] = "ingen kontroll";
+$lang["SetIP"]      = "IP tabell";
+$lang["SetTime"]    = "locking timeout";
+$lang["SetHours"]   = "timmar";
+$lang["SetOffset"]  = "Server time offset";
+$lang["SetEntry"]   = "Visa kommentarer per sida";
+$lang["SetSubmit"]  = "OK/Verkställ";
+$lang["SetEmpty"]   = "Ej godkänt värde";
+$lang["SetSort"]    = "Visa i ordning";
+$lang["SetAsc"]     = "stigande";
+$lang["SetDesc"]    = "fallande";
+$lang["Setusort"]   = "sortera inte";
+$lang["SetOptions"] = "Antal val tillåtna i nya omröstningar";
+$lang["SetPolls"]   = "Omröstningar per sida";
+
+/* Change Password */
+$lang["PwdTitle"] = "Ändra Lösenord";
+$lang["PwdText"]  = "Ändra användarnamnet eller lösenordet";
+$lang["PwdUser"]  = "Användarnamn";
+$lang["PwdPass"]  = "Lösenord";
+$lang["PwdConf"]  = "Bekräfta Lösenordet";
+$lang["PwdNoUsr"] = "Du glömde att fylla i fältet för användarnamnet";
+$lang["PwdNoPwd"] = "Du glömde att fylla i lösenords fältet";
+$lang["PwdBad"]   = "Lösenordet stämmer inte";
+
+/* Poll Stats */
+$lang["StatCrea"]  = "Skapad";
+$lang["StatAct"]   = "Aktiv";
+$lang["StatReset"] = "Nollställ logg statistiken";
+$lang["StatDis"]   = "loggning är inte i bruk för denna omröstning";
+$lang["StatTotal"] = "Antal röster";
+$lang["StatDay"]   = "röster per dag";
+
+/* Poll Comments */
+$lang["ComTotal"]  = "Antal kommentarer totalt";
+$lang["ComName"]   = "Namn";
+$lang["ComPost"]   = "skickat";
+$lang["ComDel"]    = "Är du säker att du vill radera detta meddelande?";
+
+/* Help */
+$lang["Help"]       = "Hjälp";
+$lang["HelpPoll"]   = "För att lägga in denna omröstning på en webbsida sätt in koden här nedan";
+$lang["HelpRand"]   = "Det är också möjligt att visa en omröstning slumpvis";
+$lang["HelpNew"]    = "Visa alltid senaste omröstningen";
+$lang["HelpSyntax"] = "Syntax";
+
+/* Days */
+$weekday[0] = "Söndag";
+$weekday[1] = "Måndag";
+$weekday[2] = "Tisdag";
+$weekday[3] = "Onsdag";
+$weekday[4] = "Torsdag";
+$weekday[5] = "Fredag";
+$weekday[6] = "Lördag";
+
+/* Months */
+$months[0]  = "Januari";
+$months[1]  = "Februari";
+$months[2]  = "Mars";
+$months[3]  = "April";
+$months[4]  = "Maj";
+$months[5]  = "Juni";
+$months[6]  = "Juli";
+$months[7]  = "Augusti";
+$months[8]  = "September";
+$months[9]  = "Oktober";
+$months[10] = "November";
+$months[11] = "December";
+
+/* Colors */
+$color_array[0]  = "aqua";
+$color_array[1]  = "blå";
+$color_array[2]  = "brun";
+$color_array[3]  = "mörkgrön";
+$color_array[4]  = "guld";
+$color_array[5]  = "grön";
+$color_array[6]  = "grå";
+$color_array[7]  = "orange";
+$color_array[8]  = "rosa";
+$color_array[9]  = "lila";
+$color_array[10] = "röd";
+$color_array[11] = "gul";
+
+?>
