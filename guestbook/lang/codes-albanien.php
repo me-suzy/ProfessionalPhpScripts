6e32e6feeb88eec3bfa2cd16ed4fa1c6c4e256e7
me-suzy <me.suzana@gmail.com>
2025-08-10 07:46:22 +0300

    Add guestbook/lang/codes-albanien.php

---
+++ guestbook/lang/codes-albanien.php
@@ -0,0 +1,167 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+<TITLE>Smilies And AGCodes</TITLE>
+<META content="text/html; charset=windows-1252" http-equiv=Content-Type>
+<style type="text/css">
+<!--
+td {  font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 8pt}
+-->
+</style>
+</HEAD>
+<BODY bgColor=#ffffff link=#000080 text=#000000 vLink=#000080>
+<CENTER>
+  <table width="95%" border="0" cellspacing="1" cellpadding="0">
+    <tr>
+      <td height="25"> Q K A &nbsp;&nbsp;&nbsp;J A N Ë&nbsp;&nbsp;&nbsp;S M I L I E S ?</td>
+    </tr>
+    <tr>
+      <td>
+        <p>'Smilies' (buzeqeshjet apo acid) janë shenja të vogla grafike të
+           cilat mund ti perdorni ti shprehni ndjenjat apo emocionet.
+           Nese e shfrytëzoni e-mailin apo bisedimet virtuale (chat),
+          jeni te vetedishem me konceptin e buzeqeshjes.</p>
+        <p>Ja pra lista e SMILEY-ve qe fukncionojnë momentalisht: </p>
+      </td>
+    </tr>
+  </table>
+  <table bgcolor=#f7f7f7 border=0 width="95%" cellspacing="1" cellpadding="4">
+    <tbody>
+    <tr>
+      <td bgcolor="#996699"><font color=#ffffff><b>Çka duhet shtypur</b></font></td>
+      <td bgcolor="#996699"><font color=#ffffff><b>Ndjenja që paraqet</b></font></td>
+      <td bgcolor="#996699"><font color=#ffffff><b>Grafika që do të shifet</b></font></td>
+    </tr>
+
+<?php include ("./smilies.inc"); ?>
+
+    </tbody>
+  </table>
+  <br>
+  <table width="95%" border="0" cellspacing="1" cellpadding="0">
+    <tr>
+      <td height="25">Ç K A &nbsp;&nbsp;&nbsp;Ë S H T Ë &nbsp;&nbsp;&nbsp;A G &nbsp;&nbsp;&nbsp;K O D I ? </td>
+    </tr>
+    <tr>
+      <td>
+        <p>AGkodi ëhstë një variantë e HTML kodit që dnoshta veq e njifni. Detyra e tij është që
+          ta shtosh një fukncion apo një stil porosisë tuaj që do të bashkpunoj me HTMl kodin.
+          Ju mund të përdorni AGkodin edhe në qoftë se HTMl kodi është i deaktivuar.
+
+        <p>AGkodi aktual:
+      </td>
+    </tr>
+  </table>
+<TABLE border=0 cellPadding=0 cellSpacing=0 width="95%" align="center">
+  <TBODY>
+  <TR>
+    <TD bgColor=#000000>
+      <TABLE border=0 cellPadding=4 cellSpacing=1 width="100%">
+        <TBODY>
+        <TR bgColor=#0099CC>
+          <TD><b><font color="#FFFFFF">URL Hyperlinku</font></b></TD>
+        </TR>
+        <TR bgColor=#ffffff>
+          <TD>Në qoftë se AGkodi është i aktivuar, ju nuk keni më nevojë të përdorni
+          [URL] kodin për të krijuar nji hiperlink. Ju thjesht shënoni internet adresën tuaj,
+          dhe linku do të krijohet automatikisht:
+            <UL>
+              <LI><font color="#800000">http://www.URL-e-juaj.com </font>
+              <LI><FONT color=#800000>www.URL-e-juaj.com </FONT>
+              Mbani mend se ju mund të përdorni edhe metodën e gjatë të
+              shenimit të adresës suaj http:// apo te filloni vetem me www.
+              Mirëpo në qoftë se adresa juaj nuk fillon me www atëherë ju jeni të obligueshëm
+              që të filloni me "http://". Përpos http protokolit ju mund të përdorni edhe
+              protokole tjera sikur ftp etj. kur AGkodi është i aktivuar.<BR>
+                <BR>
+              <LI>Ju gjithashtu mund të përdorni edhe hiperlinqe të maskuara me ndihpen e [url] kodit.
+              Vetem shënoni kështu:
+              <br><br>
+                <CENTER>
+                  <FONT color=#ff0000>[url=http://www.URL-e-juaj.com]</FONT>L I N K U<FONT color=#ff0000>[/url]</FONT>
+                </CENTER><br><br>
+              <LI>
+                <P>[URL] kodi i "vjetër" do të punoj sikur e cekur më poshtë. Vetëm
+                shënoni linkun sikur që është treguar në shembullin që po vijon (AGkodi është me të
+                <FONT color=#ff0000>kuqe</FONT>).
+                <P>
+                  <CENTER>
+                    <FONT color=#ff0000>[url]</FONT>http://www.URL-e-juaj.com<FONT color=#ff0000>[/url]</FONT>
+                  </CENTER>
+                <P>Në shembujt e marrur më lartë, AGkodi automatikisht e gjeneron
+                hyperlinkun drejt URL-së së cekur. Gjithashtu do të hap një dritarë të re
+                kur vizitori do të klikojë në atë link. Mbani mend se "http://" mund
+                të përdoret simbas dëshirës.
+                Në shembullin e dytë më lartë URL-ja do të lidhë textin e shënuar
+                me adresen që ju keni shkruar pas shenjër barabart "=".
+                gjithashtu mbani mend se ju nuk duhet të përdorni thojzat në URL kod.</P>
+              </LI>
+            </UL>
+          </TD>
+        <TR bgColor=#0099CC>
+          <TD><b><font color="#FFFFFF">Linqet drejt E-mailave</font></b></TD>
+        </TR>
+        <TR bgColor=#ffffff>
+          <TD>Me ia shtu një hyperlink një email adrese të shënuar në porosinë tuaj,
+          vetem shenoni adresen tuaj në mënyrën që vijon (AGkodi është me të
+            <FONT color=#ff0000>kuqe</FONT>).
+            <P>
+              <CENTER>
+                <FONT color=#ff0000>[email]</FONT>emaili-juaj@furnizuesi.com<FONT color=#ff0000>[/email]</FONT>
+              </CENTER>
+            <P>Në shembullin e marrë më lartë, AGkodi automatikisht e gjeneron hiperlinkun
+            drejt email adresës së cekur. </P>
+          </TD>
+        </TR>
+        <TR bgColor=#0099CC>
+          <TD><b><font color="#FFFFFF">Bold (shkronjat e trasha) and Italics (shkronjat e pjerRëta)</font></b></TD>
+        </TR>
+        <TR bgColor=#ffffff>
+          <TD>Ju mund te shkruani një tekst të pjerrët apo të trashni tekstin ne qoftëse
+          përdorni këtë kod: [b] [/b] apo [i] [/i].
+            <P>
+              <CENTER>
+                Hello, <FONT color=#ff0000>[b]</FONT><B>Agron</B><FONT color=#ff0000>[/b]</FONT><BR><br>
+                Hello, <FONT color=#ff0000>[i]</FONT><I>Teuta</I><font color="#FF0000">[i]</font>
+              </CENTER>
+          </TD>
+        </TR>
+        <TR bgColor=#0099CC>
+          <TD><b><font color="#FFFFFF">Vendosja e një imazhi</font></b></TD>
+        </TR>
+        <TR bgColor=#ffffff>
+          <TD>Për të shtuar një grafikë në porosinë tuaj, vetem shkruani këtë kod
+           (AGkodi është me të<FONT color=#ff0000>kuqe</FONT>):
+            <P>
+              <CENTER>
+                <FONT color=#ff0000>[img]</FONT>http://www.URL-e-juaj.com/image/foto.gif<FONT color=#ff0000>[/img]</FONT>
+              </CENTER>
+            <P>Në shembullin e marrur më lartë, AGkodi automatikisht do të bën
+            imazhin të pashëm në porosinë tuaj.
+            Mbani mend: "http://" është e domosdoshme për <FONT color=#ff0000>[img]</FONT> kodin.</P>
+          </TD>
+        </TR>
+        </TBODY>
+      </TABLE>
+    </TD>
+  </TR>
+  </TBODY>
+</TABLE>
+<table width="95%" border="0" cellspacing="1" cellpadding="4" align="center">
+  <tr>
+    <td><font color=#800000>Shenim</font><br>
+      Nuk bën të përdorni HTMl dhe AG kodin njikohësisht për të njejtin funkcion.
+      Gjithashtu për AGkodin ju mund të perdorni shkronja të mëdha apo të vogla pra:
+      <font color=#ff0000>[URL]</font> apo <font color=#ff0000>[url]</font>.<br><br>
+      <font color="#800000">Përdorimi i gabueshëm i AGkodit:</font> <br>
+       <font color="#ff0000">[url]</font> www.URL-e-juaj.com <font color=#ff0000>[/url]</font> - mos lini zbrazëtira ne mes të kllapave
+        the tekstit të cilit po ia aplikoni AG kodin.<br>
+        <br>
+        <font color="#ff0000">[email]</font>emaili-juaj@furnizuesi.com<font color=#ff0000>[email]</font> - kllapat në fund përmbajnë një thyesë (<font color=#ff0000>[/email]</font>)
+    </td>
+  </tr>
+</table>
+</CENTER>
+<BR>
+</BODY>
+</HTML>
