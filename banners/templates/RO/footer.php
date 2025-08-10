05692e04a9a7f3e1b172bbd0f83d07a312e07dd9
me-suzy <me.suzana@gmail.com>
2025-08-10 06:27:36 +0300

    Add banners/templates/RO/footer.php

---
+++ banners/templates/RO/footer.php
@@ -0,0 +1,4 @@
+<HR>
+<CENTER><small>&copy;2002 <A HREF="mailto:<? echo"$adminemail"; ?>"><? echo"$sitename"; ?></A></small><br><small><? echo"$sitename"; ?> is used at your own risk.  We cannot be held responsible for the faliure of the system, or the non-displayal of banners, or the display of an incorrect banner.</small></CENTER>
+</body>
+</html>
