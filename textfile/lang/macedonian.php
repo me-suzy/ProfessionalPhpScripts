1bcc24258521139eaaca94c7e321df1ef68e7eb0
me-suzy <me.suzana@gmail.com>
2025-08-10 11:54:53 +0300

    Add textfile/lang/macedonian.php

---
+++ textfile/lang/macedonian.php
@@ -0,0 +1,181 @@
+<?php
+# Advanced Poll Language File (Admin) #
+# Macedonian translation by Jovanovic Miroslav
+# E-mail: jmiroslav@yahoo.com
+#Date: 10/12/2001, 21:39
+
+# Charset
+$lang["charset"]   = "windows-1251";
+
+# General
+$lang["Logout"] = "Èçëåç";
+$lang["FormUndo"] = "Ïîíèøòè ãè ïðîìåíèòå";
+$lang["FromClear"] = "Ïîíèøòè";
+$lang["FormEnter"] = "Âíåñåòå âàëèäíî êîðèñíè÷êî èìå è ëîçèíêà";
+$lang["FormWrong"] = "Ïîãðåøíî êîðèñíè÷êî èìå èëè à ëîçèíêà";
+$lang["FormOK"] = "ÎÊ";
+$lang["Updated"] = "Ïðîìåíèòå ñå îñâåæåíè!";
+$lang["NoUpdate"] = "Íàñòàïè ãðåøêà! Íèêàêâè èçìåíè íå ñå íàïðàâåíè!";
+$lang["Confirm"] = "Äàëè ñòå ñèãóðíè?";
+$lang["NavNext"] = "Ñëåäíàòà ñòðàíèöà";
+$lang["NavPrev"] = "Ïðåòõîäíàòà ñòðàíèöà";
+$lang["License"] = "Ëèöåíöåí äîãîâîð";
+$lang["ScrollTxt"] = "Ïðèòèñíè ãî òàñòåðîò PAGE DOWN çà äà ãî âèäèòå îñòàòîêîò îä äîãîâîðîò.";
+
+/* Templates */
+$lang["Templates"] = "Øàáëîíè";
+$lang["tpl_exist"] = "Èìåòî íà øàáëîíîò âåå ïîñòîè.";
+$lang["tpl_new"] = "Äîäàäè íîâ ñåò íà øàáëîíè.";
+$lang["tpl_succes"] = "Øàáëîíîò å óñïåøíî äîäàäåí!";
+$lang["tpl_bad"] = "Èìåòî íà øàáëîíîò íå å âàëèäíî!";
+$lang["tpl_save"] = "Ñíèìè";
+$lang["preview"] = "Ïðåãëåä";
+$lang["newtpl"] = "Íîâ ñåò íà øàáëîíè";
+
+# Poll List
+$lang["IndexTitle"] = "Ëèñòà íà àíêåòè";
+$lang["IndexQuest"] = "Ïðàøàå";
+$lang["IndexID"] = "ID íà àíêåòàòà";
+$lang["IndexDate"] = "Äàòóì";
+$lang["IndexDays"] = "Äåíîâè";
+$lang["IndexExp"] = "Âðåìå íà òðàåå";
+$lang["IndexExpire"] = "èçìèíàò";
+$lang["IndexNever"] = "íèêîãàø";
+$lang["IndexStat"] = "Ñòàòèñòèêà";
+$lang["IndexCom"] = "Êîìåíòàðè";
+$lang["IndexAct"] = "Àêöè¼à";
+$lang["IndexDel"] = "èçáðèøè";
+
+# Create A New Poll
+$lang["NewTitle"] = "Êðåèðà¼ íîâà àíêåòà";
+$lang["NewOption"] = "Îïöè¼à";
+$lang["NewNoQue"] = "Ïîëåòî çà ïðàøàåòî íå å ïîïîëíåòî";
+$lang["NewNoOpt"] = "Ïîëåòî çà îïöè¼à íå å ïîïîëíåòî";
+
+# Poll Edit
+$lang["EditStat"] = "Ñòàòóñ";
+$lang["EditText"] = "Óðåäè ¼à îâàà íàêåòà";
+$lang["EditReset"] = "Ïîíèøòè ¼à îâàà àíêåòà";
+$lang["EditOn"] = "Àêòèâíà";
+$lang["EditOff"] = "Íåàêòèâíà";
+$lang["EditHide"] = "Îäëîæåíà";
+$lang["EditLgOff"] = "ëîãèðàåòî å èñêëó÷åíî";
+$lang["EditLgOn"] = "ëîãèðàåòî å âêëó÷åíè";
+$lang["EditAdd"] = "Äîäàäè îïöèè";
+$lang["EditNo"] = "Íèòó åäíà îïöè¼à íå å äîäàäåíà!";
+$lang["EditOk"] = "Îïöè¼àòà å äîäàåíà!";
+$lang["EditSave"] = "Ñíèìè ãè ïðîìåíèòå";
+$lang["EditOp"] = "Ìèíèìóì äâå îïöèè ñå ïîòðåáíè!";
+$lang["EditMis"] = "Ïðàøàåòî è îïöèèòå íå ñå äåôèíèðàíè!";
+$lang["EditDel"] = "Çà äà îäñòðàíèòå îïöè¼à, îñòàâåòå ãî ïîëåòî ïðàçíî";
+$lang["EditCom"] = "Äîçâîëè êîìåíòàðè";
+
+# General Settings
+$lang["SetTitle"] = "Îïøòè ïîäåñóâàà";
+$lang["SetOption"] = "Îïöèè çà òàáåëàòà, ôîíòîò è áîèòå";
+$lang["SetMisc"] = "Îñòàíàòî";
+$lang["SetText"] = "Ìîäèôèêàöè¼à íà îïøòèòå ïîäåñóâàà";
+$lang["SetURL"] = "URL äî äèðåêòîðèóìîò ñî ñëèêèòå";
+$lang["SetBURL"] = "URL äî ôîëäåðîò ñî àíêåòèòå";
+$lang["SetNo"] = "áåç êðàåí slash";
+$lang["SetLang"] = "£àçèê";
+$lang["SetPoll"] = "Íàñëîâ íà àíêåòàòà";
+$lang["SetButton"] = "Êîï÷åòî çà ãëàñàå";
+$lang["SetResult"] = "Ëèíê êîí ðåçóëòàòèòå";
+$lang["SetVoted"] = "Âåå ñòå ãëàñàëå";
+$lang["SetComment"] = "Èñïðàòåòå ãè âàøèòå êîìåíòàðè";
+$lang["SetTab"] = "Øèðèíà íà òàáåëàòà";
+$lang["SetBarh"] = "Âèñèíà íà ëåíòàòà";
+$lang["SetBarMax"] = "Ìàêñèìàëíà äîëæèíà íà ëåíòàòà";
+$lang["SetTabBg"] = "Áî¼à íà îñíîâàòà íà òàáåëàòà";
+$lang["SetFrmCol"] = "Áî¼à íà ðàìêàòà";
+$lang["SetFontCol"] = "Áî¼à íà ôîíòîò";
+$lang["SetFace"] = "Ôîíòîâè";
+$lang["SetShow"] = "Ïîêàæè";
+$lang["SetPerc"] = "ïðîöåíò";
+$lang["SetVotes"] = "ãëàñîâè";
+$lang["SetCheck"] = "Ïðîâåðêà";
+$lang["SetNoCheck"] = "áåç ïðîâåðêà";
+$lang["SetIP"] = "IP òàáåëà";
+$lang["SetTime"] = "îä åäíà IP àäðåñà íå ìîæå äà ñå ãëàñà";
+$lang["SetHours"] = "÷àñà";
+$lang["SetOffset"] = "Îäñòàïóâàå îä âðåìåòî íà ñåðâåðîò";
+$lang["SetEntry"] = "Áðî¼ íà êîìåíòàðè ïî ñòðàíèöà";
+$lang["SetSubmit"] = "Ïîòâðäè ãè ïðîìåíèòå";
+$lang["SetEmpty"] = "Íåèñïðàâíà âðåäíîñò";
+$lang["SetSort"] = "Ðàñïîðåä íà ïðèêàæóâàå";
+$lang["SetAsc"] = "ðàñòå÷êè";
+$lang["SetDesc"] = "îïàà÷êè";
+$lang["Setusort"] = "íå ñîðòèðà¼";
+$lang["SetOptions"] = "Îïöèè äîçâîëåíè âî íîâèòå àíêåòè";
+$lang["SetPolls"] = "Àíêåòè ïî ñòðàíèöà";
+
+# Change Password
+$lang["PwdTitle"] = "Ïðîìåíè ¼à ëîçèíêàòà";
+$lang["PwdText"] = "Ïðîìåíà íà êîðèñíè÷êîòî èìå èëè ëîçèíêàòà";
+$lang["PwdUser"] = "Êîðèñíè÷êî èìå";
+$lang["PwdPass"] = "Ëîçèíêà";
+$lang["PwdConf"] = "Ïîòâðäè ¼à ëîçèíêàòà";
+$lang["PwdNoUsr"] = "Ïîëåòî çà êîðèñíè÷êîòî èìå íå å ïîïîëíåòî";
+$lang["PwdNoPwd"] = "Ïîëåòî çà ëîçèíêàòà íå å ïîïîëíåòî";
+$lang["PwdBad"] = "Ëîçèíêèòå íå ñîîäâåñòóâààò";
+
+# Poll Stats
+$lang["StatCrea"] = "Êðåèðàíà";
+$lang["StatAct"] = "Àêòèâíà";
+$lang["StatReset"] = "Ðåñåòèðà¼ ãî ëîãîò íà ñòàòèñòèêèòå";
+$lang["StatDis"] = "ëîãèðàåòî íå å âëêó÷àíî çà îâàà àíêåòà";
+$lang["StatTotal"] = "Âêóïíî ãëàñîâè";
+$lang["StatDay"] = "ãëàñîâè äíåâíî";
+
+# Poll Comments
+$lang["ComTotal"] = "Âêóïíî êîìåíòàðè";
+$lang["ComName"] = "Èìå";
+$lang["ComPost"] = "èñïðàòåíî";
+$lang["ComDel"] = "Äàëè ñòå ñèãóðíè äåêà ñàêàòå äà ¼à èçáðèøåòå îâàà ïîðàêà?";
+
+# Help
+$lang["Help"] = "Ïîìîø";
+$lang["HelpPoll"] = "Çà äà ¼à äîäàäåòå àíêåòà íà ñà¼òîò êîðèñòåòå ãî îâî¼ êîä (x-îò çàìåíåòå ãî ñî áðî¼îò íà àíêåòàòà)";
+$lang["HelpRand"] = "Ìîæíî å è äà ñå ïðèêàæóâààò è àíêåòè ïî ñëó÷àåí èçáîð";
+$lang["HelpNew"] = "Ñåêîãàø ïðèêàæè ¼à íà¼íîâàòà àíêåòà";
+$lang["HelpSyntax"] = "Ñèíòàêñà";
+
+# Days
+$weekday[0] = "Íåäåëà";
+$weekday[1] = "Ïîíåäåëíèê";
+$weekday[2] = "Âòîðíèê";
+$weekday[3] = "Ñðåäà";
+$weekday[4] = "×åòâðòîê";
+$weekday[5] = "Ïåòîê";
+$weekday[6] = "Ñàáîòà";
+
+# Months
+$months[0]  = "£àíóàðè";
+$months[1]  = "Ôåáðóàðè";
+$months[2]  = "Ìàðò";
+$months[3]  = "Àïðèë";
+$months[4]  = "Ìà¼";
+$months[5]  = "£óíè";
+$months[6]  = "£óëè";
+$months[7]  = "Àâãóñò";
+$months[8]  = "Ñåïòåìâðè";
+$months[9]  = "Îêòîìâðè";
+$months[10] = "Íîåìâðè";
+$months[11] = "Äåêåìâðè";
+
+# Colors
+$color_array[0]  = "ñèíà";
+$color_array[1]  = "ïëàâà";
+$color_array[2]  = "êàôåíà";
+$color_array[3]  = "òåìíî çåëåíà";
+$color_array[4]  = "çëàòíà";
+$color_array[5]  = "çåëåíà";
+$color_array[6]  = "ñèâà";
+$color_array[7]  = "ïîðòîêàëîâà";
+$color_array[8]  = "ðîçîâà";
+$color_array[9]  = "ëèë¼àêîâà";
+$color_array[10] = "öðâåíà";
+$color_array[11] = "æîëòà";
+
+?>
