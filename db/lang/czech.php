8f0726b0f78791e585b2ae798a9862d352fd3a60
me-suzy <me.suzana@gmail.com>
2025-08-10 06:43:45 +0300

    Add db/lang/czech.php

---
+++ db/lang/czech.php
@@ -0,0 +1,177 @@
+<?php
+
+/* Charset */
+$lang["charset"]   = "windows-1250";
+
+/* General */
+$lang["Logout"]    = "Odhlásit";
+$lang["FormUndo"]  = "Obnov zmìny";
+$lang["FromClear"] = "Reset";
+$lang["FormEnter"] = "Prosím vlo správné uiv. jméno a heslo";
+$lang["FormWrong"] = "Chybné uiv. jméno nebo heslo";
+$lang["FormOK"]    = "OK";
+$lang["Updated"]   = "Zmìny byly aktualizovány!";
+$lang["NoUpdate"]  = "Nastala chyba! Nebudou provedené ádné zmìny!";
+$lang["Confirm"]   = "Jsi si jistý?";
+$lang["NavNext"]   = "Dalí stránka";
+$lang["NavPrev"]   = "Pøedchozí stránka";
+$lang["License"]   = "Licenèní podmínky";
+$lang["ScrollTxt"] = "Stiskni klávesu PAGE DOWN, aby jsi vidìl zbytek podmínek.";
+
+/* Templates */
+$lang["Templates"]  = "ablony";
+$lang["tpl_exist"]  = "Toto jméno ablony u existuje.";
+$lang["tpl_new"]    = "Pøidej novou ablonu.";
+$lang["tpl_succes"] = "Záznam byl úspìnì pøidaný!";
+$lang["tpl_bad"]    = "Jméno ablony je chybné!";
+$lang["tpl_save"]   = "Ulo";
+$lang["preview"]    = "Prohlédni";
+$lang["newtpl"]     = "Nová ablona";
+
+/* Poll List */
+$lang["IndexTitle"]  = "Seznam anket";
+$lang["IndexQuest"]  = "Otázka";
+$lang["IndexID"]     = "ID ankety";
+$lang["IndexDate"]   = "Datum";
+$lang["IndexDays"]   = "Dní";
+$lang["IndexExp"]    = "Expirace";
+$lang["IndexExpire"] = "expiruje";
+$lang["IndexNever"]  = "nikdy";
+$lang["IndexStat"]   = "statistika";
+$lang["IndexCom"]    = "Komentáøe";
+$lang["IndexAct"]    = "Akce";
+$lang["IndexDel"]    = "Odstraò";
+
+/* Create A New Poll */
+$lang["NewTitle"]  = "Vytvoø novou anketu";
+$lang["NewOption"] = "Monost";
+$lang["NewNoQue"]  = "Zapomnìl jsi vyplnit pole s otázkou";
+$lang["NewNoOpt"]  = "Zapomnìl jsi vyplnit pole s moností";
+
+/* Poll Edit */
+$lang["EditStat"]  = "Stav";
+$lang["EditText"]  = "Zmìò tuto anketu";
+$lang["EditReset"] = "Resetuj tuto anketu";
+$lang["EditOn"]    = "zapnutá";
+$lang["EditOff"]   = "vypnutá";
+$lang["EditHide"]  = "skrytá";
+$lang["EditLgOff"] = "zaka logování";
+$lang["EditLgOn"]  = "povol logování";
+$lang["EditAdd"]   = "Pøidej monost";
+$lang["EditNo"]    = "ádná monost nebyla pøidaná!";
+$lang["EditOk"]    = "Monost pøidaná!";
+$lang["EditSave"]  = "Ulo zmìny";
+$lang["EditOp"]    = "Jsou potøebné alespoò dvì monosti!";
+$lang["EditMis"]   = "Otázka a monosti nebyly definované!";
+$lang["EditDel"]   = "Pro odstranìní monosti nech pole nevyplnìné";
+$lang["EditCom"]   = "Povol komentáøe";
+
+/* General Settings */
+$lang["SetTitle"]   = "Hlavní nastavení";
+$lang["SetOption"]  = "Nastavení tabulky, fontù a barvy";
+$lang["SetMisc"]    = "Ostatní nastavenía";
+$lang["SetText"]    = "Zmìnit hlavní nastavení";
+$lang["SetURL"]     = "URL adresáøe s obrázky";
+$lang["SetBURL"]    = "URL adresáøe s anketou";
+$lang["SetNo"]      = "Bez koncového lomítka";
+$lang["SetLang"]    = "Jazyk";
+$lang["SetPoll"]    = "Titulek ankety";
+$lang["SetButton"]  = "Hlasovací tlaèítko";
+$lang["SetResult"]  = "Odkaz s výsledky";
+$lang["SetVoted"]   = "U jste hlasoval(a)";
+$lang["SetComment"] = "Polete Vá komentáø";
+$lang["SetTab"]     = "íøka tabulky";
+$lang["SetBarh"]    = "Výka pruhu";
+$lang["SetBarMax"]  = "Max. délka pruhu";
+$lang["SetTabBg"]   = "Barva pozadí tabulky";
+$lang["SetFrmCol"]  = "Barva rámu";
+$lang["SetFontCol"] = "Barva fontu";
+$lang["SetFace"]    = "Typ fontu";
+$lang["SetShow"]    = "Výsledky zobraz";
+$lang["SetPerc"]    = "procentuálnì";
+$lang["SetVotes"]   = "hlasù";
+$lang["SetCheck"]   = "Kontroluj";
+$lang["SetNoCheck"] = "bez kontroly";
+$lang["SetIP"]      = "IP tabulku";
+$lang["SetTime"]    = "doba blokování";
+$lang["SetHours"]   = "hodin";
+$lang["SetOffset"]  = "Offset èasu serveru";
+$lang["SetEntry"]   = "Poèet komentáøù na stránku";
+$lang["SetSubmit"]  = "Ulo zmìny";
+$lang["SetEmpty"]   = "Chybná hodnota";
+$lang["SetSort"]    = "Zobraz poøadí";
+$lang["SetAsc"]     = "sestupne";
+$lang["SetDesc"]    = "vzestupne";
+$lang["Setusort"]   = "netøídit";
+$lang["SetOptions"] = "Poèet moností v nových anketách";
+$lang["SetPolls"]   = "Anket na stránku";
+
+/* Change Password */
+$lang["PwdTitle"] = "Zmìò heslo";
+$lang["PwdText"]  = "Zmeò uiv. jméno nebo heslo";
+$lang["PwdUser"]  = "Uiv. jméno";
+$lang["PwdPass"]  = "Heslo";
+$lang["PwdConf"]  = "Potvrï heslo";
+$lang["PwdNoUsr"] = "Zapomnìl jste vyplnit pole s u. jménem";
+$lang["PwdNoPwd"] = "Zapomnìl jste vyplnit pole s heslem";
+$lang["PwdBad"]   = "Hesla nesoulasí";
+
+/* Poll Stats */
+$lang["StatCrea"]  = "Vytvoøené";
+$lang["StatAct"]   = "Aktivní";
+$lang["StatReset"] = "Resetuj statistiku logù";
+$lang["StatDis"]   = "logování pro tuto anketu je vypnuté";
+$lang["StatTotal"] = "Vech hlasù";
+$lang["StatDay"]   = "hlasù za den";
+
+/* Poll Comments */
+$lang["ComTotal"]  = "Vech komentáøù";
+$lang["ComName"]   = "Jméno";
+$lang["ComPost"]   = "posláno";
+$lang["ComDel"]    = "Jste si jistý, e chcete odstranit tuto zprávu?";
+
+/* Help */
+$lang["Help"]       = "Pomoc";
+$lang["HelpPoll"]   = "Pro zobrazení ankety na web stránce vlote nasledujúci kód";
+$lang["HelpRand"]   = "Takté je moné zobrazit náhodnou anketu";
+$lang["HelpNew"]    = "Vdy zobraz poslední anketu";
+$lang["HelpSyntax"] = "Syntaxe";
+
+/* Days */
+$weekday[0] = "Nedìle";
+$weekday[1] = "Pondìlí";
+$weekday[2] = "Úterý";
+$weekday[3] = "Støeda";
+$weekday[4] = "Ètvrtek";
+$weekday[5] = "Pátek";
+$weekday[6] = "Sobota";
+
+/* Months */
+$months[0]  = "Leden";
+$months[1]  = "Únor";
+$months[2]  = "Bøezen";
+$months[3]  = "Duben";
+$months[4]  = "Kvìten";
+$months[5]  = "Èerven";
+$months[6]  = "Èervenec";
+$months[7]  = "Srpen";
+$months[8]  = "Záøí";
+$months[9]  = "Øíjen";
+$months[10] = "Listopad";
+$months[11] = "Prosinec";
+
+/* Colors */
+$color_array[0]  = "svìtle modrá";
+$color_array[1]  = "modrá";
+$color_array[2]  = "hnìdá";
+$color_array[3]  = "tmavì zelená";
+$color_array[4]  = "zlatá";
+$color_array[5]  = "zelená";
+$color_array[6]  = "edá";
+$color_array[7]  = "oranová";
+$color_array[8]  = "rùová";
+$color_array[9]  = "purpurová";
+$color_array[10] = "èervená";
+$color_array[11] = "lutá";
+
+?>
