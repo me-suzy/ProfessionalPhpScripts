b292ca76c92ca69ba53b4c27ce84e0c8857bb7ab
me-suzy <me.suzana@gmail.com>
2025-08-10 06:26:57 +0300

    Add banners/adduser.php

---
+++ banners/adduser.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2//EN">
+<HTML>
+<HEAD><TITLE>Add User To Banner Network</TITLE></HEAD>
+<BODY TEXT="#000000" BGCOLOR="#FFFFFF">
+<?php
+$newuser = "<?php user( \"$username\", \"$bannerurl\", \"$clicksto\", \"$password\", \"$email\");?>";
+?>
+<table width=50%><tr><td>
+<?php
+echo"Opening users.php<!-- Error:";
+$fp = fopen("users.php", "a");
+?>--></td><td>
+<?php
+if(!$fp) die ("<font color=\"#ff0000\">Failed</font></td></tr></table>");
+else echo"<font color=\"#00ff00\">Done</font><br>";
+?></td></tr><tr><td>
+<?php
+echo"Writing to users.txt<!--Error:";
+fwrite($fp, $newuser);
+?>--></td><td>
+<?php
+echo "<font color=\"#00ff00\">Done</font><br>";
+?></td></tr><tr><td>
+<?php
+echo"Closing users.txt<!--Error:";
+fclose ($fp);
+?>--></td><td>
+<?php
+echo"<font color=\"#00ff00\">Done</font><br>";
+
+//mail("riscos@myrealbox.com","Banner Network Installation","Someone has installed the banner network.");
+
+?>
+</body>
+</html>
