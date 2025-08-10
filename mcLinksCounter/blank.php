9aae9aa8dc275b6b7ea09b794bca7b71cfada557
me-suzy <me.suzana@gmail.com>
2025-08-10 09:15:20 +0300

    Add mcLinksCounter/blank.php

---
+++ mcLinksCounter/blank.php
@@ -0,0 +1,36 @@
+<?
+// mcLinksCounter 1.2 - Marc Cagninacci - marc@phpforums.net - http://www.phpforums.net
+//
+// If you want to contribute, send me your translation!
+//
+// Translation by :
+// @ :
+// www :
+////////////////////////////////////////////////////////
+
+$lAgent= ""; // "Agent";
+$lCadre= ""; // "or frame";
+$lCat= ""; // "Category";
+$lClics= ""; // "hits";
+$lConfSuppr= ""; // "confirm deletion";
+$lErreur= ""; // "Error";
+$lFait= ""; // "Detailed datas of \"the day before yesterday\" has been deleted";
+$lHeure= ""; // "Hour";
+$lHier= ""; // "yesterday";
+$lIp= ""; // "I.P. address";
+$lLogin= ""; // "Login";
+$lModifier= ""; // "edit";
+$lModifLien= ""; // "Modify a Link";
+$lNettoyage= ""; // "Delete old datas";
+$lNom= ""; // "Name";
+$lNouveau= ""; // "New Link";
+$lOblig= ""; // "required fields";
+$lPass= ""; // "Password";
+$lRetour= ""; // "Back to Statistics";
+$lSupprimer= ""; // "delete";
+$lTarget= ""; // "Target";
+$lToday= ""; // "Today";
+$lTotal= ""; // "GENERAL TOTAL";
+$lUrl= ""; // "URL";
+$lValider= ""; // "Submit";
+?>
