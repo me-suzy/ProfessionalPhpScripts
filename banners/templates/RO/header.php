72e482e0c712d8fdcade712a98c3ca58a9d3316d
me-suzy <me.suzana@gmail.com>
2025-08-10 06:27:37 +0300

    Add banners/templates/RO/header.php

---
+++ banners/templates/RO/header.php
@@ -0,0 +1,7 @@
+<html>
+<head><title><? echo"$sitename"; ?></title>
+</head>
+<body background="img/bg.gif" text="#000000" link="#ff0000" vlink="#ff0000" alink="#ff0000" bgcolor="#dddddd">
+<?php echo $message; ?>
+<center><font color="#ff0000" size="7"><? echo"$sitename"; ?></font></center>
+<HR>
