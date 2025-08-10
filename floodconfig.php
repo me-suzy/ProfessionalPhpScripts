c496de007f19bcea19234fb182dc0bd73be93787
me-suzy <me.suzana@gmail.com>
2025-08-10 05:52:33 +0300

    Add floodconfig.php

---
+++ floodconfig.php
@@ -0,0 +1,91 @@
+<?php
+//-----------------------------------------------
+//        ENGLISH - Name of your website.
+//
+//        ESPAÑOL - Nombre de tu website.
+//
+
+$sitename = "Floodbox";
+
+//
+//-----------------------------------------------
+//        ENGLISH - Administrators, enter between 1 username and 1,000,000 usernames :P
+//                  NOTE: Every username must start with UPPERCASE must be between 
+//                        double quotes and separated by comas.
+//
+//                  TO POST AS ADMIN: In the name input write your ADMIN NAME followed by an "@"
+//				    and the password, explample:
+//    
+//                  youradminuser@yourpass    george@pass    floodboy@pass
+//
+//        ESPAÑOL - Administradores, escribe entre 1 y 1,000,000 de nombres de usuario :P
+//                  NOTA: Cada nombre de usuario debe empezar con MAYUSCULAS, debe de estar
+//					entre comillas dobles y separados por comas.
+//
+//                  PARA PUBLICAR COMO ADMINISTRADOR: en el lugar donde debes escribir tu
+//                  nombre de usuario, escribe tu nombre de administrador seguido por una "@"
+//                  y por el password, por ejemplo;
+//
+//                  nombredeadministrador@tupass    george@pass    floodboy@pass
+//
+
+$admin = array("George","Jorge","Floodboy","Flood"); 
+
+//
+//-----------------------------------------------
+//        ENGLISH - Enter your global admin password
+//
+//        ESPAÑOL - Escribe el password global para los Administradores
+//
+
+$password = "pass";
+
+//-----------------------------------------------
+//        ENGLISH - This is the time (in seconds) between posts (60 = 1 min), (600 = 10 min), ....
+//
+//        ESPAÑOL - Este es el tiempo (en segundos) entre un post y otro (60 = 1 min), (600 = 10 min), ....
+//
+
+$floodtime = 30;
+
+//-----------------------------------------------
+//        ENGLISH - Max number of characters in post
+//
+//        ESPAÑOL - Maximo numero de caracteres en un post
+//
+
+$maxchar = 101;
+
+//-----------------------------------------------
+//        ENGLISH - Max number of characters in usernames
+//
+//        ESPAÑOL - Maximo numero de caracteres en nombres de usuario
+//
+
+$namemaxchar = 11;
+
+//----------------------------------------------
+//        ENGLISH - Max number of characters in one sigle word
+//                  (This will brake all those laaaaaaaaaaaaaaaarge words)
+//
+//        ESPAÑOL - Maximo numero de caracteres en una sola palabra
+//                  (Esto va a partir esas laaaaaaaaaaaaaaaargas palabras)
+//
+
+$maxlenword = 16;
+
+//----------------------------------------------
+//        ENGLISH - Banned words ;-)
+//                  NOTE: Enter your banned words only with lowercase
+//
+//        ESPAÑOL - Palabras no permitidas ;-)
+//                  NOTA: Escribe las palabras no permitidas en minusculas
+//
+
+$badwords = array("fuck","suck","shit","joder");
+
+//
+//  That's it :D ENJOY !
+//
+//  Eso es todo :D ENJOY !
+?>
