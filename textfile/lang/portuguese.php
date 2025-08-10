668b573f9c8adf6748520e3a57b47d7c02599c5b
me-suzy <me.suzana@gmail.com>
2025-08-10 11:54:59 +0300

    Add textfile/lang/portuguese.php

---
+++ textfile/lang/portuguese.php
@@ -0,0 +1,177 @@
+<?php
+
+/* Charset */
+$lang["charset"]   = "iso-8859-1";
+
+/* General */
+$lang["Logout"]    = "Sair";
+$lang["FormUndo"]  = "Restaurar Mudanças";
+$lang["FromClear"] = "RESET";
+$lang["FormEnter"] = "Por favor, escreve um username e uma password válida";
+$lang["FormWrong"] = "Username ou Password inválido";
+$lang["FormOK"]    = "OK";
+$lang["Updated"]   = "As mudanças foram efectuadas com sucesso!!";
+$lang["NoUpdate"]  = "Ocorreu um erro! Não foram efectuadas mudanças!";
+$lang["Confirm"]   = "Tens a certeza?";
+$lang["NavNext"]   = "Próxima Página";
+$lang["NavPrev"]   = "Página Anterior";
+$lang["License"]   = "Termos de Utilização";
+$lang["ScrollTxt"] = "Puxa o texto para baixo para ler o resto dos termos.";
+
+/* Templates */
+$lang["Templates"]  = "Templates";
+$lang["tpl_exist"]  = "Já existe o nome que escolheste.";
+$lang["tpl_new"]    = "Adiciona uma nova template.";
+$lang["tpl_succes"] = "Foi adicionado com sucesso!"; 
+$lang["tpl_bad"]    = "O nome da template é inválido!";
+$lang["tpl_save"]   = "Salvar";
+$lang["preview"]    = "Prever";
+$lang["newtpl"]     = "Nova Template";
+
+/* Poll List */
+$lang["IndexTitle"]  = "Lista de Sondagens";
+$lang["IndexQuest"]  = "Pergunta";
+$lang["IndexID"]     = "ID";
+$lang["IndexDate"]   = "Data";
+$lang["IndexDays"]   = "Dias";
+$lang["IndexExp"]    = "Expiração";
+$lang["IndexExpire"] = "expirada";
+$lang["IndexNever"]  = "nunca";
+$lang["IndexStat"]   = "Estatísticas";
+$lang["IndexCom"]    = "Comentários";
+$lang["IndexAct"]    = "Acção";
+$lang["IndexDel"]    = "apagar";
+
+/* Create A New Poll */
+$lang["NewTitle"]  = "Nova Sondagem";
+$lang["NewOption"] = "Opção";
+$lang["NewNoQue"]  = "Esqueceste de preencher a pergunta";
+$lang["NewNoOpt"]  = "Esqueceste de preencher as opções";
+
+/* Poll Edit */
+$lang["EditStat"]  = "Estado";
+$lang["EditText"]  = "Editar esta sondagem";
+$lang["EditReset"] = "Voltar a 0";
+$lang["EditOn"]    = "activo";
+$lang["EditOff"]   = "desactivo";
+$lang["EditHide"]  = "hidden";
+$lang["EditLgOff"] = "logging off";
+$lang["EditLgOn"]  = "logging on";
+$lang["EditAdd"]   = "Adicionar Opções";
+$lang["EditNo"]    = "Não foram adicionadas novas opções!";
+$lang["EditOk"]    = "Opções adicionadas!";
+$lang["EditSave"]  = "Salvar Alterações";
+$lang["EditOp"]    = "São OBRIGATÓRIAS pelo menos 2 opções!";
+$lang["EditMis"]   = "A Pergunta e as Opções não foram definidas!";
+$lang["EditDel"]   = "Para remover uma opção deixa o campo em branco";
+$lang["EditCom"]   = "Permite Comentários?";
+
+/* General Settings */
+$lang["SetTitle"]   = "Configurações Gerais";
+$lang["SetOption"]  = "Tabelas, Fontes e Cores";
+$lang["SetMisc"]    = "Vários";
+$lang["SetText"]    = "Configurações Gerais";
+$lang["SetURL"]     = "URL para o directório das imagens";
+$lang["SetBURL"]    = "URL para o directório da sondagem";
+$lang["SetNo"]      = "No trailing slash";
+$lang["SetLang"]    = "Idioma";
+$lang["SetPoll"]    = "Título da Sondagem";
+$lang["SetButton"]  = "Botão de Voto";
+$lang["SetResult"]  = "Link dos Resultados";
+$lang["SetVoted"]   = "Para quem já votou...";
+$lang["SetComment"] = "Comentários";
+$lang["SetTab"]     = "Largura da Tabela";
+$lang["SetBarh"]    = "Altura da Barra";
+$lang["SetBarMax"]  = "Max. bar length";
+$lang["SetTabBg"]   = "Cor: Fundo da Tabela";
+$lang["SetFrmCol"]  = "Cor do Título";
+$lang["SetFontCol"] = "Font color";
+$lang["SetFace"]    = "Font face";
+$lang["SetShow"]    = "Display result as";
+$lang["SetPerc"]    = "percentagem";
+$lang["SetVotes"]   = "nº de votos";
+$lang["SetCheck"]   = "Verificar";
+$lang["SetNoCheck"] = "no checking";
+$lang["SetIP"]      = "IP table";
+$lang["SetTime"]    = "tempo entre votos";
+$lang["SetHours"]   = "horas";
+$lang["SetOffset"]  = "Server time offset";
+$lang["SetEntry"]   = "Comentário por página";
+$lang["SetSubmit"]  = "Submeter Alteraçõess";
+$lang["SetEmpty"]   = "Valor Inválido";
+$lang["SetSort"]    = "Ordem";
+$lang["SetAsc"]     = "Ascendente";
+$lang["SetDesc"]    = "Descendente";
+$lang["Setusort"]   = "nao ordenar";
+$lang["SetOptions"] = "Número de Opções permitidas por sondagem";
+$lang["SetPolls"]   = "Sondagens por página";
+
+/* Change Password */
+$lang["PwdTitle"] = "Mudar Password";
+$lang["PwdText"]  = "Mudar o USERNAME ou a PASSWORD";
+$lang["PwdUser"]  = "Username";
+$lang["PwdPass"]  = "Password";
+$lang["PwdConf"]  = "Confirmar Password";
+$lang["PwdNoUsr"] = "Esqueceste-te de preencher o campo do USERNAME";
+$lang["PwdNoPwd"] = "Esqueceste-te de preencher o campo da PASSWORD";
+$lang["PwdBad"]   = "A confirmação da password não condiz";
+
+/* Poll Stats */
+$lang["StatCrea"]  = "Criada a";
+$lang["StatAct"]   = "Activa";
+$lang["StatReset"] = "Recomeçar o Log das Estatísticas";
+$lang["StatDis"]   = "logging is disabled for this poll";
+$lang["StatTotal"] = "Total de Votos";
+$lang["StatDay"]   = "votos por dia";
+
+/* Poll Comments */
+$lang["ComTotal"]  = "Total de Comentários";
+$lang["ComName"]   = "Nome";
+$lang["ComPost"]   = "colocada";
+$lang["ComDel"]    = "Tebs a certeza que queres apagar este comentário?";
+
+/* Help */
+$lang["Help"]       = "Ajuda";
+$lang["HelpPoll"]   = "To emded a poll into a webpage insert the code snippet from below";
+$lang["HelpRand"]   = "Também é possível colocar uma sondagem aleatória";
+$lang["HelpNew"]    = "Mostra sempre a última sondagem";
+$lang["HelpSyntax"] = "Syntax";
+
+/* Days */
+$weekday[0] = "Domingo";
+$weekday[1] = "Segunda";
+$weekday[2] = "Terça";
+$weekday[3] = "Quarta";
+$weekday[4] = "Quinta";
+$weekday[5] = "Sexta";
+$weekday[6] = "Sábado";
+
+/* Months */
+$months[0]  = "Janeiro";
+$months[1]  = "Fevereiro";
+$months[2]  = "Março";
+$months[3]  = "Abril";
+$months[4]  = "Maio";
+$months[5]  = "Junho";
+$months[6]  = "Julho";
+$months[7]  = "Agosto";
+$months[8]  = "Setembro";
+$months[9]  = "Outubro";
+$months[10] = "Novembro";
+$months[11] = "Dezembro";
+
+/* translating this array does not change the reference */
+$color_array[0]  = "azul claro";
+$color_array[1]  = "azul escuro";
+$color_array[2]  = "castanho";
+$color_array[3]  = "verde escuro";
+$color_array[4]  = "ouro";
+$color_array[5]  = "verde claro";
+$color_array[6]  = "cinzento";
+$color_array[7]  = "laranja";
+$color_array[8]  = "rosa";
+$color_array[9]  = "violeta";
+$color_array[10] = "vermelho";
+$color_array[11] = "amarelo";
+
+?>
