e86ca996cbe8986af42da43e76a4a3d5e01e2718
me-suzy <me.suzana@gmail.com>
2025-08-10 09:15:32 +0300

    Add mcLinksCounter/french.php

---
+++ mcLinksCounter/french.php
@@ -0,0 +1,29 @@
+<?
+// mcLinksCounter 1.2 - Marc Cagninacci - marc@phpforums.net - http://www.phpforums.net
+
+$lAgent= "Agent";
+$lCadre= "ou cadre";
+$lCat= "Cat&eacute;gorie";
+$lClics= "clics";
+$lConfSuppr= "confirmer suppression";
+$lErreur= "Erreur";
+$lFait= "Les donn&eacute;es d&eacute;taill&eacute;es ant&eacute;rieures &agrave; hier ont &eacute;t&eacute; effac&eacute;es";
+$lHeure= "Heure";
+$lHier= "hier";
+$lIp= " Adresse I.P.";
+$lLogin= "Identifiant";
+$lModifier= "modifier";
+$lModifLien= "Modifier un Lien";
+$lNettoyage= "Nettoyage Table";
+$lNom= "Nom";
+$lNouveau= "Nouveau Lien";
+$lOblig= "champs obligatoires";
+$lPass= "Mot de Passe";
+$lRetour= "Retour Statistiques";
+$lSupprimer= "supprimer";
+$lTarget= "Target";
+$lToday= "Ce jour";
+$lTotal= "TOTAL GENERAL";
+$lUrl= "URL";
+$lValider= "Valider";
+?>
