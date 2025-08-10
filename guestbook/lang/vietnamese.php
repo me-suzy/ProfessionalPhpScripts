e9959b4ccf231a3a06a0d4558549ec02466c5342
me-suzy <me.suzana@gmail.com>
2025-08-10 07:49:05 +0300

    Add guestbook/lang/vietnamese.php

---
+++ guestbook/lang/vietnamese.php
@@ -0,0 +1,106 @@
+<?php
+# General
+
+$LANG["metatag"]    = "<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">";
+
+$LANG["BookMess1"]  = "HTML code is disabled";
+$LANG["BookMess2"]  = "HTML code b&#7853;t";
+$LANG["BookMess3"]  = "Ghi s&#7893; l&#432;u niêm";
+$LANG["BookMess4"]  = "Xem s&#7893; l&#432;u ni&#7879;m";
+$LANG["BookMess5"]  = "Qu&#7843;n tr&#7883;";
+$LANG["BookMess6"]  = "Cám &#417;n b&#7841;n &#273;ã &#273;&#7871;n th&#259;m trang web c&#7911;a Nh&#7885; Nhem, m&#7901;i b&#7841;n hãy
+l&#432;u gi&#7919; bút tích c&#7911;a mình t&#7841;i &#273;ây";
+$LANG["BookMess7"]  = "L&#7901;i bình";
+$LANG["BookMess8"]  = "Tr&#7903; l&#7841;i Form";
+$LANG["BookMess9"]  = "B&#7841;n hãy vi&#7871;t vài dòng c&#7843;m t&#432;&#7903;ng nhé.";
+$LANG["BookMess10"] = "<u>Cám &#417;n b&#7841;n &#273;ã ghi s&#7893; l&#432;u ni&#7879;m c&#7911;a Nh&#7885; Nhem,.</u><br><br>B&#7841;n s&#7869; &#273;&#432;&#7907;c &#273;&#432;a tr&#7903; l&#7841;i xem s&#7893; l&#432;u ni&#7879;m trong vòng 2 giây
+n&#7919;a.";
+
+$LANG["EmailAdminSubject"] = "Vi&#7871;t &#273;ôi dòng nh&#7855;n g&#7917;i";
+$LANG["EmailGuestSubject"] = "Cam on ban da ghi so luu niem cua Nho Nhem";
+
+$LANG["AltIP"]    = "IP logged";
+$LANG["AltICQ"]   = "Send ICQ Message";
+$LANG["AltEmail"] = "Send E-mail";
+$LANG["AltUrl"]   = "View Web Page";
+$LANG["AltAim"]   = "Instant Messenger";
+$LANG["AltCom"]   = "Write a comment";
+
+# Form
+$LANG["FormMess1"]   = "&#272;&#7875; vi&#7871;t vào s&#7893; l&#432;u ni&#7879;m, m&#7901;i b&#7841;n &#273;i&#7873;n vào form d&#432;&#7899;i &#273;ây.
+Các m&#7909;c có d&#7845;u * là b&#7855;t bu&#7897;c.";
+$LANG["FormMess2"]   = "Smilies b&#7853;t";
+$LANG["FormMess3"]   = "AGCode b&#7853;t";
+$LANG["FormMess4"]   = "Chú thích";
+$LANG["FormMess5"]   = "D&#432;&#7899;i &#273;ây là n&#7897;i dung b&#7841;n &#273;ã vi&#7871;t.";
+$LANG["FormMess6"]   = "AG Code is OFF";
+$LANG["FormMess7"]   = "Smilies are OFF";
+$LANG["FormName"]    = "Tên c&#7911;a b&#7841;n";
+$LANG["FormEmail"]   = "E-mail";
+$LANG["FormLoc"]     = "N&#417;i sinh s&#7889;ng";
+$LANG["FormPic"]     = "G&#7917;i kèm &#7843;nh";
+$LANG["FormUrl"]     = "Trang ch&#7911;";
+$LANG["FormGender"]  = "Gender";
+$LANG["FormMale"]    = "Male";
+$LANG["FormFemale"]  = "Female";
+$LANG["FormMessage"] = "B&#7841;n vi&#7871;t gì nào?";
+$LANG["FormSelect"]  = "S&#7893; l&#432;u ni&#7879;m";
+$LANG["FormUser"]    = "Username";
+$LANG["FormPass"]    = "Password";
+$LANG["FormReset"]   = "Reset";
+$LANG["FormSubmit"]  = "Submit";
+$LANG["FormPreview"] = "Preview";
+$LANG["FormBack"]    = "Quay tr&#7903; l&#7841;i";
+$LANG["FormEnter"]   = "Khu v&#7921;c qu&#7843;n tr&#7883; S&#7893; L&#432;u Ni&#7879;m. B&#7841;n hãy cung c&#7845;p Tên và M&#7853;t
+kh&#7849;u qu&#7843;n tr&#7883;!";
+$LANG["FormButton"]  = "Go";
+$LANG["FormPriv"]    = "Tin riêng";
+
+# Navigation Bar
+$LANG["NavTotal"]   = "T&#7893;ng s&#7889; ý ki&#7871;n:";
+$LANG["NavRecords"] = "S&#7889; bài vi&#7871;t trên m&#7895;i trang là:";
+$LANG["NavPrev"]    = "Trang tr&#432;&#7899;c";
+$LANG["NavNext"]    = "Trang sau";
+
+# Post Errors
+$LANG["ErrorPost1"]  = "Ban quen khong dien ten";
+$LANG["ErrorPost2"]  = "Ban quen khong dien noi dung bai viet";
+$LANG["ErrorPost3"]  = "Bai viet co the qua ngan hoac qua dai";
+$LANG["ErrorPost4"]  = "One of the input fields does not seem to be valid.";
+$LANG["ErrorPost5"]  = "Có l&#7895;i...";
+$LANG["ErrorPost6"]  = "File &#7843;nh quá l&#7899;n";
+$LANG["ErrorPost7"]  = "Wrong image file type";
+$LANG["ErrorPost8"]  = "Sorry! We have flood-control activated. Please try again after a period of time elapses!";
+$LANG["ErrorPost9"]  = "Your IP adress is banned!";
+$LANG["ErrorPost10"] = "N&#7897;i dung b&#7841;n vi&#7871;t có ch&#7913;a t&#7915; b&#7883; c&#7845;m, b&#7841;n hãy ki&#7875;m tra và
+g&#7917;i l&#7841;i nhé";
+$LANG["ErrorPost11"] = "B&#7841;n quên không &#273;i&#7873;n m&#7909;c N&#7897;i dung";
+
+$LANG["PassMess1"] = "B&#7841;n hãy &#273;i&#7873;n tên và m&#7853;t kh&#7849;u:";
+$LANG["PassMess2"] = "Tên và m&#7853;t kh&#7849;u sai.";
+$LANG["PassMess3"] = "Xin l&#7895;i sai m&#7853;t kh&#7849;u.";
+
+# Days
+$weekday[0] = "Ch&#7911; nh&#7853;t";
+$weekday[1] = "Th&#7913; 2";
+$weekday[2] = "Th&#7913; 3";
+$weekday[3] = "Th&#7913; 4";
+$weekday[4] = "Th&#7913; 5";
+$weekday[5] = "Th&#7913; 6";
+$weekday[6] = "Th&#7913; 7";
+
+# Months
+$months[0]  = "tháng 1";
+$months[1]  = "tháng 2";
+$months[2]  = "tháng 3";
+$months[3]  = "tháng 4";
+$months[4]  = "tháng 5";
+$months[5]  = "tháng 6";
+$months[6]  = "tháng 7";
+$months[7]  = "tháng 8";
+$months[8]  = "tháng 9";
+$months[9]  = "tháng 10";
+$months[10] = "tháng 11";
+$months[11] = "tháng 12";
+
+?>
